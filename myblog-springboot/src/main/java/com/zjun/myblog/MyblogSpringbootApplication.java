0ecf093f3adc681733bc93e7daa781d7197ed0d8
zjun <yuanzj2000@gmail.com>
2024-12-27 12:34:16 +0800

    msg

---
+++ myblog-springboot/src/main/java/com/zjun/myblog/MyblogSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.zjun.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogSpringbootApplication.class, args);
+    }
+
+}
