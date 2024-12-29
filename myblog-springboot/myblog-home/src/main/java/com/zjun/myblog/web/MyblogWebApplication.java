9ddf9d32967e382e5dc846b385004417189da39a
zjun <yuanzj2000@gmail.com>
2024-12-29 19:56:52 +0800

    create common and security

--- myblog-springboot/myblog-home/src/main/java/com/zjun/myblog/web/MyblogWebApplication.java
+++ myblog-springboot/myblog-home/src/main/java/com/zjun/myblog/web/MyblogWebApplication.java
@@ -3,11 +3,9 @@ package com.zjun.myblog.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.zjun.myblog.*")
 public class MyblogWebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyblogWebApplication.class, args);
     }
-
 }
